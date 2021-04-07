9efc4d266e9af488e3ec073d8db07f891b1f3956
humayunahmed8 <humayunfarhad3@gmail.com>
2021-04-07 22:30:33 +0600

    for and while loop added

---
+++ for-loop.js
@@ -0,0 +1,9 @@
+// For Loop 
+// লুপ মানে হচ্ছে একটা জিনিস বার বার করা। 
+
+var myFriends = ['rahim', 'karim', 'salam', 'kalam', 'jabbar', 'barkat'];
+
+for(var i = 0; i < myFriends.length; i++){
+    var element = myFriends[i]; 
+    console.log(element);
+}
