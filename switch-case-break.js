830b84a74ddf7f1fc7312d638d53e39c5f1589dd
humayunahmed8 <humayunfarhad3@gmail.com>
2021-04-07 23:02:16 +0600

    add JS switch case

---
+++ switch-case-break.js
@@ -0,0 +1,20 @@
+// Javascript Switch Case 
+
+var number = 550;
+
+switch (number) {
+    case 400:
+        console.log('I am 400');
+        break;
+    case 550:
+        console.log('I am 550');
+        break;
+    case 600: 
+        console.log('I am 600');
+        break;
+    case 540:
+        console.log('I am 550');
+        break;
+    default: 
+        console.log('Dont worry!');
+}
