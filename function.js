781d05fbeeb0122e588d0dfbb47ce040c6f05369
humayunahmed8 <humayunfarhad3@gmail.com>
2021-04-08 22:59:03 +0600

    define a function and how to call object

---
+++ function.js
@@ -0,0 +1,23 @@
+// Javascript Function 
+
+
+function add(number1, number2){
+    var totalNumber = number1 + number2;
+    return totalNumber;
+}
+
+var sum = add(5, 5);
+
+console.log(sum);
+
+
+/*
+
+Beginners can follow this learning path to learn JavaScript -
+Step 1: Complete w3schools JS Tutorials.
+Step 2: Come back to my channel and complete my Play with JavaScript DOM series.
+Step 3: Complete my Think in a JavaScript way playlist.
+Step 4: Complete my Modern JavaScript Playlist. This will prepare you for JavaScript frameworks and library.
+Step 5: Now you can take your decision whether you want to become a front-end developer or a back-end developer. If you want to be a front-end developer, you can take the React JS path and learn it from my React Tutorial Series. If you want to take the back-end path, you can learn Node.js from my Node.js Tutorial Series. If you want to be a full-stack developer, you can learn both.
+
+*/
